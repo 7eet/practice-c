928cf772c07351b0e7e1e54e66d1528b3e7d0154
7eet <37106933+7eet@users.noreply.github.com>
2018-03-21 21:40:40 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
+int factorial(int num);
 int main()
 {
-  int i,a;
-  long fact;
-  printf("enter number:_");
+  int a;
+  long facto;
+  printf("enter number: ");
   scanf("%d",&a);
   if (a==0)
 	printf("factorial of 0 is 0.");
   else
-    fact=1;
-    for (i=1;i<=a;)
-     {
-       
-       fact=fact*i;
-       
-       i=i+1;
-       printf("%li\n",fact);
-     }
+    if (a<0)
+      printf("Sorry! factorial of negative is not possible");
+    else
+      facto=factorial(a);
+      printf("Factorial of %d is %li .",a,facto); 	
   return 0;
+}
+int factorial(int num) 
+{
+  int i,a;
+  long fact;   
+  fact=1;
+  for (i=1;i<=a;)
+  {
+    fact=fact*i;
+    i=i+1;
+    return fact;
+  }
 }
